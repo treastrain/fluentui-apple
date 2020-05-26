93598b1b1a9f82d7be6e5fb72cd6f89da0b04325
treastrain / Tanaka Ryoga <tr@tret.jp>
2020-05-26 14:54:30 +0900

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "FluentUI",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v11),
+        // .macOS(.v10_13),
+    ],
+    pkgConfig: nil,
+    providers: nil,
+    products: [
+        .library(
+            name: "FluentUI",
+            targets: ["FluentUI"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "FluentUI",
+            dependencies: [],
+            path: "ios/FluentUI",
+            exclude: [
+                "Info.plist",
+                "Resources/Localization/CultureMapping.json",
+            ],
+            sources: nil,
+            resources: nil,
+            publicHeadersPath: nil,
+            cSettings: nil,
+            cxxSettings: nil,
+            swiftSettings: nil,
+            linkerSettings: nil)
+    ],
+    swiftLanguageVersions: [.v5],
+    cLanguageStandard: nil,
+    cxxLanguageStandard: nil
+)
